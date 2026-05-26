978a6d8ee8883fad544ba8f28a5cf4d4dd24a6e2
Piotr Kozlowski <pegaz@ante.pl>
2026-05-26 08:53:46 +0200

    feat: order to internal transport and internal transport

--- features/shared/utils/useGetAppPathForOrdersAll.tsx
+++ features/shared/utils/useGetAppPathForOrdersAll.tsx
@@ -5,13 +5,15 @@ import {
   GREENHOUSE_CROPS,
   GREENHOUSE_CROPS_WORKS,
   INDEX,
+  POTTED_PLANTS,
+  POTTED_PLANTS_WORKS,
 } from "../types/interfaces-navigation";
 import AppPath from "../ui/app-path/AppPath";
 
 export const useGetAppPathForOrdersAll = () => {
   const getAppPathForOrdersAll = (
     submoduleName: string,
-    whatOrderType: AllCropsOrdersSubmodules
+    whatOrderType: AllCropsOrdersSubmodules,
   ) => {
     const basePath = getBasePath(whatOrderType);
 
@@ -23,9 +25,25 @@ export const useGetAppPathForOrdersAll = () => {
   };
 
   function getBasePath(whatOrderType: AllCropsOrdersSubmodules) {
-    if (whatOrderType === "greenhouse_crops_works_order_to_spacing")
+    if (
+      whatOrderType === "greenhouse_crops_works_order_to_spacing" ||
+      whatOrderType === "greenhouse_crops_works_order_export_to_customer"
+    )
       return [INDEX, GREENHOUSE_CROPS, GREENHOUSE_CROPS_WORKS];
-    return [INDEX, FIELD_CROPS, FIELD_CROPS_WORKS];
+
+    if (whatOrderType === "potted_plants_works_order_to_internal_transport")
+      return [INDEX, POTTED_PLANTS, POTTED_PLANTS_WORKS];
+
+    if (
+      whatOrderType === "field_crops_works_internal_transport" ||
+      whatOrderType === "field_crops_works_order_to_hardener" ||
+      whatOrderType === "field_crops_works_order_export_to_customer"
+    )
+      return [INDEX, FIELD_CROPS, FIELD_CROPS_WORKS];
+
+    throw new Error(
+      "useGetAppPathForOrdersAll -> getBasePath -> whatOrderType is not valid",
+    );
   }
 
   ////hook return
