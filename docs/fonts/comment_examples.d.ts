92515b13a0ab73c262a16fbde4eb13282ec24f4a
Piotr Kozlowski <pegaz@ante.pl>
2026-06-15 15:58:23 +0200

    feat: get rid of inner generate_docs script and went to lib installed from github

---
+++ docs/fonts/comment_examples.d.ts
@@ -0,0 +1,41 @@
+export {};
+/**
+ * @public
+ * @topic
+ * PROCEDURA:
+ */
+/**
+ * @public
+ * @procedureDescription
+ * Wyświetlany jest aktualnie pikowany Zpek.
+ * Należy wybrać kolor z dostępnych kolorów dla ZP.
+ * Skanowanie tac i podpinanie ich do wybranego koloru.
+ * Na końcu podanie wypikowanej ilości i wysyłka.
+ */
+/**
+ * @public
+ * @transformApiItem
+ * wysyłka - custom api:
+ * <b>{{URL}}</b>/api.php/REST/custom/<b>risecount</b>
+ * dane - array obiektów:
+ * {
+ *  sordid: number
+ *  ordnmb: string
+ *  stk_id: string
+ *  lckcnt: number
+ *  scanned_raw_value: string
+ * }
+ * @separator
+ */
+/**
+ * @public
+ * @procedureItem
+ * raporty:
+ * @readFile `features/shared/data-access/edocReport_ProtectiveTreatments.ts`
+ */
+/**
+ * @public
+ * @guard
+ * Jeżeli taca była już wcześniej zeskanowana - info + koniec procedury.
+ */
+//# sourceMappingURL=comment_examples.d.ts.map
